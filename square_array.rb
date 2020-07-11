ec62712ba039080991008d94d9068e6dc452f9ae
Brandon Williams <nis350ztt@gmail.com>
2020-07-11 05:06:35 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,3 @@
 def square_array(array)
-  new_array = []
   puts "collect array : #{array.collect {|numbers| ** 2}}\n\n"
-  array.each do |numbers| 
-  puts new_array << numbers ** 2
-  end
-  return new_array
 end
